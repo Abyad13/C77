e507477ed02b9fff1efa3f0f3e8c2e93601c1636
Abyad13 <118222272+Abyad13@users.noreply.github.com>
2023-05-22 21:33:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("my first console project");
+var firstname="ranbeer";
+var lastname="kapoor";
+var fullname= firstname.concat(lastname);
+console.log (fullname);
+var a=10;
+var b=5;
+var add=a+b;
+console.log (add);
+var sub=a-b;
+console.log (sub);
+var multiple=a*b;
+console.log(multiple);
+var division=a/b;
+console.log (division);
